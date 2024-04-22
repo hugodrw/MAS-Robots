10414da2ad00d4213b61ddd3bafc19d230ee00ba
hugodrw <hugoderohanwillner@gmail.com>
2024-04-22 17:41:47 +0200

    more clean

--- robots/robots/model.py
+++ robots/robots/model.py
@@ -1,14 +1,3 @@
-"""
-Wolf-Sheep Predation Model
-================================
-
-Replication of the model found in NetLogo:
-    Wilensky, U. (1997). NetLogo Wolf Sheep Predation model.
-    http://ccl.northwestern.edu/netlogo/models/WolfSheepPredation.
-    Center for Connected Learning and Computer-Based Modeling,
-    Northwestern University, Evanston, IL.
-"""
-
 import mesa
 
 from .agents import Waste, Robot, Grid_Tile
